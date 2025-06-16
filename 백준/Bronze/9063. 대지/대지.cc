c711b7438585ad244808dc90b7b9760c677aa226
Subeen Jo <59586769+jsb0813@users.noreply.github.com>
2025-06-16 15:58:51 +0900

    [Bronze III] Title: 대지, Time: 36 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/9063. 대지/대지.cc
+++ 백준/Bronze/9063. 대지/대지.cc
@@ -3,16 +3,15 @@ using namespace std;
 
 int main()
 {
-    int dot[100000][2]={};
-    int n,xmin=10000,xmax=-10000,ymin=10000,ymax=-10000;
+    int n,x,y,xmin=10000,xmax=-10000,ymin=10000,ymax=-10000;
     cin>>n;
     for(int i=0;i<n;i++)
     {
-        cin>>dot[i][0]>>dot[i][1];
-        if(dot[i][0]>xmax) xmax=dot[i][0];
-        if(dot[i][0]<xmin) xmin=dot[i][0];
-        if(dot[i][1]>ymax) ymax=dot[i][1];
-        if(dot[i][1]<ymin) ymin=dot[i][1];
+        cin>>x>>y;
+        if(x>xmax) xmax=x;
+        if(x<xmin) xmin=x;
+        if(y>ymax) ymax=y;
+        if(y<ymin) ymin=y;
     }
     cout<<(xmax-xmin)*(ymax-ymin);
 }
